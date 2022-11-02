bfef8e029e5459f791e89ff73a68129146079896
corian-jal <luclemence@uchicago.edu>
2022-11-01 21:06:46 -0500

    resolved map_component and map_test dependecies, waiting on player merge for player dependencies

--- appGeo/src/tests/map_tests.ts
+++ appGeo/src/tests/map_tests.ts
@@ -1,9 +1,11 @@
 import { QUnit } from "qunit"; //import is from "qunit", where is it located?
 import { bubble } from "src/app/map/map.component";
-//import player
+// import { Player } from  --when player is merged
+
+QUnit.module("Bubble Tests");
 
 //assumes bubble variables are not private
-QUnit.test("Bubble Initialization", function(assert) {
+QUnit.test("Bubble Initialization Tests", function(assert) {
     let testbub = new bubble("testbub", 0, 10, 5, 15);
 
     assert.equal(testbub.ID, "testbub", "bubble_init should set id as testbub");
@@ -12,13 +14,13 @@ QUnit.test("Bubble Initialization", function(assert) {
     assert.equal(testbub.y_lB, 5, "y_lb should be 5");
     assert.equal(testbub.y_uB, 15, "y_up should be 15");
     assert.equal(testbub.arrlen, 0, "player list should be empty");
-    assert.equal(testbub.getPlayers(), null, "player list should be empty");
+    assert.equal(testbub.returnPlayers(), null, "player list should be empty");
 });
 
-QUnit.test("Bubble Updates", function(assert) {
+QUnit.test("Bubble Update Tests", function(assert) {
     let testbub = new bubble("testbub", 0, 15, 0, 15);
     assert.equal(testbub.arrlen, 0, "player list should be empty");
-    assert.equal(testbub.getPlayers(), null, "player list should be empty");
+    assert.equal(testbub.returnPlayers(), null, "player list should be empty");
 
     let testPlayer = new Player();
     //function that sets player location outside of bubble
@@ -32,7 +34,7 @@ QUnit.test("Bubble Updates", function(assert) {
 
     assert.true(testbub.removePlayer(testPlayer), "player should be removed from bubble");
     assert.equal(testbub.arrlen, 0, "player list should be empty");
-    assert.equal(testbub.getPlayers(), null, "player list should be empty");
+    assert.equal(testbub.returnPlayers(), null, "player list should be empty");
 
     /* Should addPlayer check inBubble(Player)? Or should inBubble call addPlayer and removePlayer?
         >> inBubble, addPlayer, and removePlayer should not call each other but be called in game loop */
