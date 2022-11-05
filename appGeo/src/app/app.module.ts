ee62c70d4bcb619998cc08093f40b73c128e478c
Nanci Diaz <116527589+nd1az@users.noreply.github.com>
2022-11-05 16:47:35 -0500

    Made the component files
    Angular cli is now functional! I have thus added the component folders to edit in. don't mind the code written inside, it automatically generated in there, just edit as you see fit.

--- appGeo/src/app/app.module.ts
+++ appGeo/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptModule } from '@nativescript/angular'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { GameComponent } from './game/game.component'
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, PlayerComponent, GameComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
