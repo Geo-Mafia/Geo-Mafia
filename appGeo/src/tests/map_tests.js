b771716eeca337ca3e8ea752b331a833ea2401c6
corian-jal <luclemence@uchicago.edu>
2022-11-07 10:24:55 -0600

    made bubble-player tests more rigorous

--- appGeo/src/tests/map_tests.js
+++ appGeo/src/tests/map_tests.js
@@ -47,15 +47,27 @@ QUnit.test("Bubble-Player Tests", function(assert) {
 
     let loc = [20, 20];
     let testPlayer = new Player(12, Test, loc, true);
-    
-    assert.true(testbub.addPlayer(testPlayer), "player should be added to bubble");
+    assert.true(testbub.addPlayer(testPlayer), "player1 should be added to bubble");
     assert.equal(testbub.List.size(), 1, "player list should have one element");
     assert.equal(testbub.List[0].userID, 12, "player added should be identified by id 12");
     assert.equal(testbub.returnPlayers()[0].userID, 12, "player added should be identified by id 12");
 
-    assert.true(testbub.removePlayer(testPlayer), "player should be removed from bubble");
+    let testPlayer2 = new Player(13, Test2, loc, true);
+    assert.true(testbub.addPlayer(testPlayer2), "player2 should be added to bubble");
+    assert.equal(testbub.List.size(), 2, "player list should have two elements");
+    assert.equal(testbub.List[1].userID, 13, "player added should be identified by id 13");
+    assert.equal(testbub.returnPlayers()[1].userID, 13, "player added should be identified by id 13");    
+
+    assert.true(testbub.removePlayer(testPlayer), "player1 should be removed from bubble");
+    assert.equal(testbub.List.size(), 1, "player list should have one element");
+    assert.equal(testbub.List[0].userID, 13, "player in list should be identified by id 13");
+    assert.equal(testbub.returnPlayers()[0].userID, 13, "player in list should be identified by id 13");
+
+    assert.true(testbub.removePlayer(testPlayer2), "player2 should be removed from bubble");
     assert.equal(testbub.List.size(), 0, "player list should be empty");
     assert.equal(testbub.returnPlayers(), null, "player list should be empty");
+
+    assert.false(testbub.removePlayer(testPlayer), "player list should already be empty");
 });
 
 /* Should addPlayer check inBubble(Player)? Or should inBubble call addPlayer and removePlayer?
