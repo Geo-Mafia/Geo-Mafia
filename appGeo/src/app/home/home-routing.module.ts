922e6d7fcff34c4960c481a2a17ab03ac9572a93
Nanci <nanci@uchicago.edu>
2022-11-14 22:27:07 -0600

    deleting the added info.plist portion because I'm pretty sure that should be not in here(?)

--- appGeo/src/app/home/home-routing.module.ts
+++ appGeo/src/app/home/home-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 import { CampusMap } from '../map/campus-map.component'
-import {Bubble} from '../map/map.component'
 
 import { HomeComponent } from './home.component'
 
