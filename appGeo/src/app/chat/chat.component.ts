09f5dd3898ad479cab753b26501451ba394582e3
Strongbrother1 <calvinmaycelee@gmail.com>
2022-11-14 21:56:41 -0600

    Working button w/ backend

--- appGeo/src/app/chat/chat.component.ts
+++ appGeo/src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { databaseAdd, databaseGet } from '../../modules/database'
+import { firebase } from "@nativescript/firebase";
 
 @Component({
   selector: 'Chat',
@@ -7,12 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Chat implements OnInit {
 
-  constructor() { }
+  public chats: String[]; //change String to any or Message class later
+
+
+  constructor() { 
+    this.chats = [];
+  }
 
   ngOnInit(): void {
+    this.chats = this.getMsgs();
+    firebase.addValueEventListener(this.updateMsg, "game/chats");
+  }
+
+  getMsgs() {
+    let msgs = [];
+    databaseGet('game/chats').then(value => {
+      msgs.concat(value);
+      console.log("all msgs: " + msgs);
+    });
+    return msgs;
+  }
+
+  updateMsg(data: object) {
+    var list = data["value"];
+    this.chats = list;
   }
 
 }
 
-export class Message{
-}
+// export class Message{
+// }
