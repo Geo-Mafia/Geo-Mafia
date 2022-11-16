d82f5821726926fbe43f05380dcc31d89a4700f7
Strongbrother1 <calvinmaycelee@gmail.com>
2022-11-15 19:47:28 -0600

    Fixed compilation error, but 'go to chat' crashes

--- appGeo/src/main.ts
+++ appGeo/src/main.ts
@@ -1,10 +1,11 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
-
+import { firebase } from "@nativescript/firebase";
 import { AppModule } from './app/app.module';
 
 //console.log('runnnnnnnnnnnnnnnnnnnnning');
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
 });
-
+firebase.init()
+firebase.addValueEventListener(console.log, "game/users")
 //console.log("doneneee");
