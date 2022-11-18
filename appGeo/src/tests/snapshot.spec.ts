97dc0d484d334731087e7dcccdb0f7bba7f4bc0b
corian-jal <luclemence@uchicago.edu>
2022-11-18 11:28:58 -0600

    fixed snapshot errors so it can compile + run tests

--- appGeo/src/tests/snapshot.spec.ts
+++ appGeo/src/tests/snapshot.spec.ts
@@ -29,15 +29,9 @@ QUnit.test("Testing receiving data", function(assert) {
     const snap = new Snapshot(1, bub);
 
     assert.equal(snap.getSnapshotID(), 1, "snapshot id should be 1");
-    assert.equal(snap.getSnapshotBubbleId(), "Bubble", "should have id: Bubble");
+    assert.equal(snap.getSnapshotBubbleID(), "Bubble", "should have id: Bubble");
 
-    const player_map = new Map(
-        snap.getSnapshotContent().map(object => {
-          return [object.getUserID(), object];
-        }),
-      );
-
-    assert.true(player_map.has(13), "snapshot should have player in bubble with id 13");
+    assert.true(snap.snapshot_content.includes("Jack"), "Jack should be included in the list of players");
     assert.notDeepEqual(snap.snapshot_content, ['Jack'], "should show that the property of arrays is same");
     
     //just need to assure that a date object is initialized
@@ -47,31 +41,23 @@ QUnit.test("Testing receiving data", function(assert) {
     assert.true(bub.addPlayer(Mark), "player should be added to original bubble");
     assert.true(bub.removePlayer(Jack), "player should be removed from original bubble");
 
-    const player_map2 = new Map(
-      snap.getSnapshotContent().map(object => {
-        return [object.getUserID(), object];
-      }),
-    );
-
-    assert.true(player_map2.has(13), "original player should still be in snapshot");
-    assert.false(player_map2.has(25), "new player should not be in snapshot");
-
-    assert.true(snap.snapshot_content.includes(Jack), "Jack should be included in the list of players")
-    assert.false(snap.snapshot_content.includes(Mark), "Mark should not be in the content bubbble");
+    assert.true(snap.snapshot_content.includes("Jack"), "Jack should be included in the list of players");
+    assert.false(snap.snapshot_content.includes("Mark"), "Mark should not be in the content bubbble");
+    assert.notDeepEqual(snap.snapshot_content, ['Jack'], "should show that the property of arrays is same");
 });
 
 QUnit.test("Testing depth of name Array", function(assert){
   const bubb = new Bubble()
   bubb.init_bubble("Bubble", 0, 0, 0, 0)
-  bubb.addPlayer(player1)
+  bubb.addPlayer(Jack)
   assert.propContains(bubb.returnPlayers, {Player: {username : 'Jack'} }, "should check if Jack is found/contained inside the array of usernames")
 
   const snap = new Snapshot(1, bubb);
   //checks if the additions changed the bubble's playerArray
   //alogside the snapshot_content array, they both are represented differently
   //now but need to make sure the snapshot_content is not affected by bubble changes
-  assert.equal(snap.getSnapshotContent().includes('Jack'),1,"Should show that Jack's username is the content array")
-  bubb.addPlayer(Mark)
-  assert.false(snap.getSnapshotContent().includes('Mark'),"Mark should not be found in this array")
-  assert.true(bubb.playerArray.includes(Mark), "should count Mark present in this array now")
+  assert.equal(snap.getSnapshotContent().includes('Jack'),1,"Should show that Jack's username is the content array");
+  bubb.addPlayer(Mark);
+  assert.false(snap.getSnapshotContent().includes('Mark'),"Mark should not be found in this array");
+  assert.true(bubb.playerArray.includes(Mark), "should count Mark present in this array now");
 })
