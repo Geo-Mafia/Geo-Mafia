d2c4b9ea64e2c830936a91958012b1bc3bc90f4f
Nanci <nanci@uchicago.edu>
2022-11-20 16:17:29 -0600

    testing if specific point is found in map

--- appGeo/src/tests/campus-map.spec.ts
+++ appGeo/src/tests/campus-map.spec.ts
@@ -64,6 +64,10 @@ QUnit.test("testing the display variable and playerInBubble", function(assert){
   P1.location = notInB
   testMap.playerInBubble(P1)
   assert.deepEqual(testMap.display.id, 'The Outside of any assigned Campuse Buildings', "should have recognized the player is Off Campus")
+  var checking = new Player()
+  checking.init(1, "PoT_To", {longitude: 41.78495048653673, latitude: -87.59665817689283}, 1)
+  testMap.playerInBubble(checking)
+  assert.deepEqual(testMap.display.id, "Woodlawn Dining Commons", "should recognize that player is in wood")
 
 })
 
