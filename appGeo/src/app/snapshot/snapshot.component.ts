56d7906a09893ecf75bdef7b38cb9e52c0b8baa6
唐苏珺 <tangsujun@OneOnly.local>
2022-11-21 15:05:58 -0600

    created new files and implemented everything needed for the UI besides what's in the snapshot.component.html file

--- appGeo/src/app/snapshot/snapshot.component.ts
+++ appGeo/src/app/snapshot/snapshot.component.ts
@@ -1,6 +1,6 @@
 import {Bubble} from '../map/map.component';
 import { Player } from '../player/player.component';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
 import { databaseAdd, databaseGet, databaseEventListener } from '../../modules/database'
 import { firebase } from "@nativescript/firebase";
 import { fromObject, ScrollView, ScrollEventData} from '@nativescript/core';
@@ -11,17 +11,59 @@ import { fromObject, ScrollView, ScrollEventData} from '@nativescript/core';
   styleUrls: ['./snapshot.component.css']
 })
 
+export class SnapshotComponent implements OnInit {
+  public snapshots: Array<any>;
+
+  constructor() { 
+    this.snapshots = [];
+  }
+
+  ngOnInit(): void {
+    this.snapshots = this.getSnapshots();
+    databaseEventListener("game/snapshots", this.updateSnapshots.bind(this));
+    console.log("got to snapshot");
+  }
+
+  getSnapshots() {
+    let snaps = [];
+    databaseGet('game/snapshots').then(value => {
+      console.log("val: " + JSON.stringify(value));
+
+      if (value == null) {
+        const bub0 = new Bubble();
+        bub0.init_bubble("Bubble0", 0, 0, 0, 0);
+        let temp = new Snapshot(0, bub0)
+        snaps = [temp];
+        databaseAdd('game/snapshots', snaps);
+      } 
+      else {
+        snaps = value; 
+      }
+    });
+    console.log("in get msgs")
+    return snaps;
+  } 
+
+  updateSnapshots(data: object) {
+    this.snapshots = [];
+    console.log("data: " + JSON.stringify(data));
+    let list = data["value"];
+    this.snapshots = list;
+  }
+}
+
+
 /* functionally, snapshot should be identified by an id and save a copy of the state of
    the state of the bubble at the time of a murder, specifically the players in the bubble.
    assuming that dead players are not in bubbles(? verify), it would be a direct saving
    of the bubble with no changes. */
 export class Snapshot {
-    snapshot_id: number;
-    snapshot_bubble_id: string
-    snapshot_content: Array<string>; //returning array of names for easy display
+    snapshot_id: Number;
+    snapshot_bubble_id: String
+    snapshot_content: Array<String>; //returning array of names for easy display
     snapshot_time: String
 
-    constructor(id: number, content: Bubble){
+    constructor(id: Number, content: Bubble){
       const init_date = new Date();
       this.snapshot_time = init_date.toLocaleString(); //a string that shows the MM/DD/YYY, HH:MM:SS format
       this.snapshot_id = id;
---
+++ appGeo/src/app/snapshot/snapshot.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SnapshotComponent } from './snapshot.component';
+import { NativeScriptFormsModule } from "@nativescript/angular";
+import { NativeScriptModule } from "@nativescript/angular";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NativeScriptModule,
+    NativeScriptFormsModule
+  ],
+  declarations: [
+    SnapshotComponent
+  ]
+})
+export class SnapshotModule {}
