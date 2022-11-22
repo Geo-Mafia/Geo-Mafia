50abba16711234da472f3a7dd69ff2918498035c
Nanci Diaz <116527589+nd1az@users.noreply.github.com>
2022-11-22 07:49:44 -0600

    letting dead players see the list
    a coldhearted move to not let them see the list

--- appGeo/src/app/map/campus-map.component.ts
+++ appGeo/src/app/map/campus-map.component.ts
@@ -293,9 +293,9 @@ export class CampusMap implements OnInit {
       //when this is called on a bubble if true will change the bubble to display to the Player
       if(checkIfIn.inBubble(pToCheck) && checkIfIn.List.has(pToCheck.userID)){
         if (!pToCheck.alive){
-          this.display = new Bubble()
-          this.playerlist = []
           checkIfIn.removePlayer(pToCheck)
+          this.display = checkIfIn
+          this.playerlist = checkIfIn.playerArray
           return
         }
       } else if(checkIfIn.inBubble(pToCheck) && !checkIfIn.List.has(pToCheck.userID)){ //should have more logic to remove a player that is in said bubble List but not in the bubble boundary
@@ -303,8 +303,8 @@ export class CampusMap implements OnInit {
           this.offcampus.removePlayer(pToCheck)
         }
         if (!pToCheck.alive){
-          this.display = new Bubble()
-          this.playerlist = []
+          this.display = checkIfIn
+          this.playerlist = checkIfIn.playerArray
           return
         }else {
           checkIfIn.addPlayer(pToCheck)
