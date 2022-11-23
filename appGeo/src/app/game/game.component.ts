03a1b5985c67e7828a6a40b40aeb6d769755e92d
Calvin Lee <77907241+Strongbrother1@users.noreply.github.com>
2022-11-23 01:54:51 -0800

    Fix Unit Tests

--- appGeo/src/app/game/game.component.ts
+++ appGeo/src/app/game/game.component.ts
@@ -455,7 +455,7 @@ export class Game implements OnInit {
 
   addPlayer(player) {
       this.players.set(player.getUserID(), player)
-      global.playerlist.set(player.getUserID(), player);
+      //global.playerlist.set(player.getUserID(), player);
       return SUCCESS;
   }
 
